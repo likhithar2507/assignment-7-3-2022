0448556672216d587d721e932357af3760ccb5d9
likhithar2507 <73028560+likhithar2507@users.noreply.github.com>
2022-03-07 19:18:57 +0530

    assignment (7-03-2022)

---
+++ square and cube.py
@@ -0,0 +1,5 @@
+y=[int(i) for i in input("Enter the numbers")]
+Num_Square = list(map(lambda i: i**2, y))
+Num_Cube = list(map(lambda i: i**3, y))
+print(" the Square of the  numbers=", Num_Square)
+print(" the Cube of  the numbers= ", Num_Cube)
